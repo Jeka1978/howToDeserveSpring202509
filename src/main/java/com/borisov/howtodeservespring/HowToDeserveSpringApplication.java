b0a40bc6beb150d242191e572ba4de39267ebdec
Evgeny_Borisov <bsevgeny@gmail.com>
2025-09-10 07:47:52 -0500

    historical service updated

--- src/main/java/com/borisov/howtodeservespring/HowToDeserveSpringApplication.java
+++ src/main/java/com/borisov/howtodeservespring/HowToDeserveSpringApplication.java
@@ -8,9 +8,12 @@ public class HowToDeserveSpringApplication {
 
     public static void main(String[] args) {
 
-//        ObjectFactory.getInstance().createObject(GameMaster.class).fight();
+        GameMaster gameMaster1 = ObjectFactory.getInstance().createObject(GameMaster.class);
+        GameMaster gameMaster2 = ObjectFactory.getInstance().createObject(GameMaster.class);
+        System.out.println(gameMaster1==gameMaster2);
+
         ApplicationContext applicationContext = new ApplicationContext("com.borisov.howtodeservespring");
-        applicationContext.getObject(GameMaster.class).fight();
+//        applicationContext.getObject(GameMaster.class).fight();
 
 
     }
