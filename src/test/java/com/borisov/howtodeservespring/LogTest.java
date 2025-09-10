b329bfa547ba77ba100342ec7d1f55853280fd8f
Tolkachev Kirill <lavcraft@gmail.com>
2025-09-10 17:49:15 +0300

    feat: add homework for LogTest

--- src/test/java/com/borisov/howtodeservespring/LogTest.java
+++ src/test/java/com/borisov/howtodeservespring/LogTest.java
@@ -17,22 +17,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 })
 public class LogTest {
     @NoArgsConstructor
-    public static class MetaSpiderForTest {
+    public static class MetaSpiderForTest implements MyClassWithLogAnnotationInterface {
         @PostConstruct
         public void init() {
             System.out.println("I'm meta spider with N lives");
         }
+
+        @Override
+        public void tryLog() {
+            System.out.println("Super logging method, try enable it");
+        }
     }
 
     public static interface MyClassWithLogAnnotationInterface {
+        //TODO Попробуйте реализовать логирование, триггером к которому является аннотация над методом интерфейса
+        //Место для вашей аннотации
         void tryLog();
     }
 
     @Test
     void should_log_when_spider_initialized(CapturedOutput capturedOutput) {
+        //TODO подумайте какие проблемы могут возникать из указанного способа создания контекста
+        // 1. Как это можно починить?
+        // 2. А ещё как? (подумайте двух способах и их плюсах и минусах)
+        //given
         ApplicationContext applicationContext = new ApplicationContext("com.borisov.howtodeservespring");
 
+        //when
         MetaSpiderForTest object = applicationContext.getObject(MetaSpiderForTest.class);
+
+        //then
         assertThat(object).isNotNull();
         assertThat(capturedOutput.getAll()).contains("I'm meta spider with N live");
     }
