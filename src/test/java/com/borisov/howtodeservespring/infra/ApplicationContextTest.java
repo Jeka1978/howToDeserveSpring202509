072aaf4c00a3e568de4179e14ea8d148867849e2
Tolkachev Kirill <lavcraft@gmail.com>
2025-09-12 13:59:36 +0300

    chore: add home tasks for ApplicationContextTest

--- src/test/java/com/borisov/howtodeservespring/infra/ApplicationContextTest.java
+++ src/test/java/com/borisov/howtodeservespring/infra/ApplicationContextTest.java
@@ -13,17 +13,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 import org.reflections.scanners.SubTypesScanner;
+import org.springframework.boot.autoconfigure.session.JdbcSessionDataSourceScriptDatabaseInitializer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ApplicationContextTest {
     @InjectMocks ApplicationContext applicationContext;
-    @Spy         Reflections        reflections = new Reflections("com.borisov.howtodeservespring", new SubTypesScanner(),new ResourcesScanner());
+    @Spy         Reflections        reflections = new Reflections("com.borisov.howtodeservespring");
     @Mock        ObjectFactory      objectFactory;
 
 
+    /***************************************************
+     *** START Test environment configuration **********
+     **************************************************/
     public interface ApContextTestInterface {
 
     }
@@ -43,6 +48,10 @@ class ApplicationContextTest {
 
     public class InjectedTestClass {
     }
+
+    /*************************************************
+     *** END Test environment configuration **********
+     ************************************************/
 
     @Test
     void should_get_object_by_interface_singleton() {
@@ -78,25 +87,46 @@ class ApplicationContextTest {
         when(objectFactory.createObject(ApContextTestSingletonObject.class)).thenAnswer(invocationOnMock -> new ApContextTestSingletonObject());
 
         //expect
+        assertThat(applicationContext.getObject(ApContextTestSingletonObject.class))
+                .as("All singleton objects are the same with object pulled by interface")
+                .isSameAs(applicationContext.getObject(ApContextTestSingletonObject.class));
+    }
+
+    //TODO домашнее задание - реализуйте ApplicationContext таким образом, чтобы возвращался один и тот же конкретный объект
+    @Test
+    @Disabled("Advanced homework")
+    void should_get_singleton_object_via_interface_and_class_are_the_same() {
+        when(objectFactory.createObject(ApContextTestSingletonObject.class)).thenAnswer(invocationOnMock -> new ApContextTestSingletonObject());
+
+        //expect
         ApContextTestSingletonObject    object  = applicationContext.getObject(ApContextTestSingletonObject.class);
-        ApContextTestSingletonObject    object2 = applicationContext.getObject(ApContextTestSingletonObject.class);
-        ApContextTestSingletonInterface object3 = applicationContext.getObject(ApContextTestSingletonInterface.class);
+        ApContextTestSingletonInterface object2 = applicationContext.getObject(ApContextTestSingletonInterface.class);
 
         assertThat(object)
-                .as("All singleton objects are the same")
-                .isSameAs(object2)
-                .isSameAs(object3);
+                .as("All singleton objects are the same with object pulled by interface")
+                .isSameAs(object2);
+    }
+
+    @Test
+    void should_get_new_object_everytime_when_no_singleton() {
+        when(objectFactory.createObject(ApContextTestObject.class)).thenAnswer(invocationOnMock -> new ApContextTestObject());
+
+        ApContextTestInterface object  = applicationContext.getObject(ApContextTestInterface.class);
+        ApContextTestInterface object2 = applicationContext.getObject(ApContextTestInterface.class);
+
+        assertThat(object)
+                .as("Prototype object should be new each time")
+                .isNotSameAs(object2);
     }
 
     @Test
     void should_get_object_configured_by_injection_configurator() {
         when(objectFactory.createObject(ApContextTestObject.class)).thenAnswer(invocationOnMock -> new ApContextTestObject());
 
-        ApContextTestObject object = (ApContextTestObject) applicationContext.getObject(ApContextTestInterface.class);
+        applicationContext.getObject(ApContextTestInterface.class);
 
+        //TODO достаточно ли проверить что метод create вызвался один раз?
+        // - Подумайте, какие ещё тесты стоит написать чтобы "система" тестов для этой логики была полна
         verify(objectFactory, times(1)).createObject(ApContextTestObject.class);
-//        assertThat(object.test)
-//                .as("Should configured field of injected object")
-//                .isNotNull();
     }
 }
